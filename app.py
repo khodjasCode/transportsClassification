9fbbb7d8a5adbb728ac127a77c3c1b58d2b798f0
khodjja <161064276+khodjasCode@users.noreply.github.com>
2025-01-26 00:37:59 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import pathlib
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+#title
+st.title('Transportni klassifikatsiya qiluvchi model!')
+
+#rasm joylash
+file = st.file_uploader('Rasm yuklash', type= ['png', 'jpeg', 'gif', 'svg'])
+st.image(file)
+
+#PIL image
+if file is not None:
+    img = PILImage.create(file)
+else:
+    st.warning("Klassifikatsiya uchun avval rasm yuklang!")
+
+#model
+model = load_learner('transport_model.pkl')
+
+#prediction
+pred, pred_id, probs = model.predict(img)
+st.success(f'It is {pred.lower()}!')
+st.info(f'Probability: {probs[pred_id]*100:.1f}%')
+
