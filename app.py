bf14a143557f9f2ff4eee192526d20e0951f9283
khodjja <161064276+khodjasCode@users.noreply.github.com>
2025-01-26 01:22:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from fastai.vision.all import *
 import plotly.express as px
 import platform
+from PIL import Image
 
 plt = platform.system()
 if plt == 'Linux': pathlib.WindowsPath = pathlib.PosixPath
@@ -12,7 +13,9 @@ st.title('Transportni klassifikatsiya qiluvchi model!')
 
 #rasm joylash
 file = st.file_uploader('Rasm yuklash', type= ['png', 'jpeg', 'gif', 'svg'])
-st.image(file)
+if file is not None:
+    img = Image.open(file)
+    st.image(img, caption='Yuklagan rasmingiz:', use_column_width=True)
 
 #PIL image
 if file is not None:
